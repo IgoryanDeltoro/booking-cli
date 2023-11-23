34dc2fa3efcbb4fdf8e5db75ea0aeccde73ebf0d
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-11-23 15:55:36 +0100

    ADDED: handler to get arrays and  unite them

--- controllers/apartments/getApartmentById.js
+++ controllers/apartments/getApartmentById.js
@@ -1,17 +1,42 @@
-const { Apartment } = require('../../models');
-
+const { Apartment, Review, User } = require('../../models');
 const { HttpError } = require('../../helpers');
+const { default: mongoose } = require('mongoose');
 
 const getApartmentsById = async (req, res) => {
- const { id } = req.params;
+  const { id } = req.params;
 
-  const result = await Apartment.findById(id, '-createdAt -updatedAt');
+  let result = [];
 
-  if (!result) {
+  const apartment = await Apartment.findById(id, '-createdAt -updatedAt');
+  const feedbacks = await Review.findById({ _id: id });
+
+  if (!apartment) {
     throw HttpError(404, `Apartment with ${id} not found`);
   }
 
-  res.json(result);
+  if (feedbacks) {
+    const { reviews } = feedbacks;
+
+    const userId = reviews.map(
+      ({ userId }) => new mongoose.Types.ObjectId(userId)
+    );
+
+    const users = await User.find({ _id: { $in: userId } });
+
+    result = reviews.map(review => {
+      const user = users.find(el => el._id.toString() === review.userId);
+
+      return {
+        id: review.id,
+        author: user.name,
+        email: user.email,
+        avatar: user.avatarURL,
+        comment: review.comment,
+      };
+    });
+  }
+
+  res.json({ ...apartment._doc, reviews: result });
 };
 
 module.exports = getApartmentsById;
