cf15ab93fb4f2926303a881a8c77a094d55eb9dc
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-04 15:51:47 +0100

    ADDED: img upload  possibility

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,6 @@
 const isValidId = require('./isValidId');
-
 const authenticate = require('./authenticate');
-
 const validateFormNameBody = require('./isValidUserName');
-
-
 const upload = require('./upload');
 
 module.exports = { isValidId, upload, authenticate, validateFormNameBody };
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -15,15 +15,21 @@ const storage = new CloudinaryStorage({
     let folder;
     if (file.fieldname === 'avatar') {
       folder = 'avatars';
-    } else if (file.fieldname === 'recipeImg') {
-      folder = 'recipeImg';
+    } else if (file.fieldname === 'Apartment') {
+      folder = 'Apartments';
     } else {
       folder = 'misc';
     }
+    const { originalname } = file;
+    const cutExtraWords = originalname
+      .split('')
+      .splice(0, originalname.length - 4)
+      .join('');
+
     return {
       folder: folder,
       allowed_formats: ['jpg', 'png', 'webp'],
-      public_id: file.originalname,
+      public_id: cutExtraWords,
       transformation: [{ aspect_ratio: '1.0', width: 350, crop: 'fill' }],
     };
   },
