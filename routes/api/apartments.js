a5e35e83a0541d19c39778548a77e052d721efba
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 19:21:21 +0100

    REMOVED: authentication from home page

--- routes/api/apartments.js
+++ routes/api/apartments.js
@@ -3,13 +3,10 @@ const apartments = require('../../controllers/apartments');
 const { authenticate } = require('../../middlewares');
 const isValid = require('../../middlewares/isValidId');
 
-
 const router = express.Router();
 
-router.use(authenticate);
-
 router.get('/', apartments.getApartments);
-router.get('/:id', isValid, apartments.getApartmentById);
-router.post('/:id/reviews',isValid, apartments.createReview);
+router.get('/:id', authenticate, isValid, apartments.getApartmentById);
+router.post('/:id/reviews', authenticate, isValid, apartments.createReview);
 
 module.exports = router;
--- routes/api/cities.js
+++ routes/api/cities.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const cities = require('../../controllers/cities');
-const { authenticate } = require('../../middlewares');
-
 const router = express.Router();
-router.use(authenticate);
 
 router.get('/', cities.getCities);
 
